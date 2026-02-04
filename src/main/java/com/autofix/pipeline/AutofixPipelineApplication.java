e1ff07dd9d6a119014815b80b3ed4e66f2378029
Siddhant45-hub <siddhantv903@gmail.com>
2026-02-04 15:03:04 +0530

    fix: correct spotless plugin configuration

--- src/main/java/com/autofix/pipeline/AutofixPipelineApplication.java
+++ src/main/java/com/autofix/pipeline/AutofixPipelineApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AutofixPipelineApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AutofixPipelineApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(AutofixPipelineApplication.class, args);
+  }
 }
